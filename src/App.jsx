d038ef46fb90e80a4c2e191ec0143c43c73bc275
DenGolivets <oxytechmusic@gmail.com>
2024-06-08 11:15:08 +0300

    game site

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.scss'
+import Header from './components/header/Header'
+import Home from './pages/Home'
+
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/effect-fade'
+
+function App() {
+
+
+  return (
+    <div className='App'>
+      <Header />
+      <Home />
+    </div>
+  )
+}
+
+export default App
